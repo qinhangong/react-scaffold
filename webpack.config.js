c101c5c4f43c7305292ff9e6356f91aa05c861d2
liangzhouren <zhangyulei68@163.com>
2018-06-12 23:45:44 +0800

    hotmodulereplace --fix

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,17 @@ const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const extractCSS = new ExtractTextPlugin('dist/css.antd.css');
-const extractLESS = new ExtractTextPlugin('dist/css.app.css');
+const extractCSS = new ExtractTextPlugin({
+    filename: 'css/public.css',
+    disable: process.env.NODE_ENV === 'development' ? true : false,
+});
+const extractLESS = new ExtractTextPlugin({
+    filename: 'css/app.css',
+    disable: process.env.NODE_ENV === 'development' ? true : false,
+});
 
 module.exports = {
-    mode: 'development',
+    mode: process.env.NODE_ENV || 'development',
     entry: './src/index.js',
     output: {
         filename: 'bundle.js',
@@ -21,17 +27,24 @@ module.exports = {
                 loader: 'babel-loader',
             }, {
                 test: /\.css/,
-                use: extractCSS.extract(['css-loader'])
+                use: extractCSS.extract({
+                    fallback: "style-loader",
+                    use: 'css-loader',
+                }),
             }, {
                 test: /\.less/,
-                use:extractLESS.extract([
-                        { loader: 'css-loader', options: { modules: true,autoprefixer:false } },
-                        "postcss-loader",
-                        'less-loader',
-                ])
-            },{
-                test:/\.(png|jpg|svg)$/,
-                loader:'url-loader?limit=5000'
+                exclude: /node_modules/,
+                use: extractLESS.extract({
+                    fallback: "style-loader",
+                    use: [
+                        { loader: 'css-loader', options: { modules: true, autoprefixer: false } },
+                        { loader: "postcss-loader" },
+                        { loader: "less-loader" }
+                    ]
+                })
+            }, {
+                test: /\.(png|jpg|svg)$/,
+                loader: 'url-loader?limit=5000'
             }
         ]
     },
@@ -39,7 +52,8 @@ module.exports = {
         port: 8888,
         contentBase: path.join(__dirname, "dist"),
         open: true,
-        hot:true,
+        hot: true,
+        inline: true,
         historyApiFallback: true
     },
     plugins: [
